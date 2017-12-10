dc829be5067399b43863f9802a93b9cc5c8320a4
Alshareef-Zaid <33426012+Alshareef-Zaid@users.noreply.github.com>
2017-12-10 19:53:30 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,38 +2,67 @@
 Zaid H Alshareef
 C00210415
 25/09/2017
-2.Factorial Calculator
+4.Electrical Resistance of a Wire
 */
 
-
-#include<iostream>
-
-
+#include <iostream>
+#include<math.h>
 using namespace std;
 
 
-    /**Main function*/
+
+    double resistance(double r,double l,double p)
+    {
+    /**equations*/
+    double resistance,A;
+    A=M_PI*pow(r,2);
+    resistance=p*(1/A);
+    cout.precision(2);
+    return resistance;
+    }
+
+    /** Main function**/
     int main()
 {
     /**Variable declaration*/
-    int num, factorial=1, n;
+    double r,l,p, result;
 
-    cout <<"Factorial Calculator\n" << endl;
+    cout << "Electrical Resistance of a Wire\n" << endl;
 
-    /**the number should be greater than or equal to 0*/
-    cout<<"Enter an integer number:";
-    cin>>num;
-
-    /**(note that 0!=1)*/
-    for (n=1; n<=num ;n++)
+    do
     {
-
-    /**formula*/
-    factorial=factorial*n;
+    /**enter values for radius*/
+    cout << "Please Enter the radius of the Wire in m:";
+    cin >>r;
     }
+    while (r<=0);
 
-    /** Calculate and display result*/
-    cout<<"The factorial is = "<<factorial<<endl;
+    do
+    {
+    /**enter values for length*/
+    cout << "Please Enter the length of the Wire in m:";
+    cin >>l;
+    }
+    while (l<=0);
 
-    return 0;
+    do
+    {
+    /**enter values for resistivity*/
+    cout << "Please Enter the resistivity of the Wire in Ohm's.m:";
+    cin >> p;
+    }
+    while (p<=0);
+
+    /**equation*/
+    result=resistance(r,l,p);
+
+    /**Display the result*/
+    cout << "The electrical resistance of wire entered is: "<< result<< endl;
+    cout << "\nconditions:" <<endl;
+    cout << "Radius is:" << r <<"m"<<endl;
+    cout << "Length is:" << l <<"m" <<endl;
+    cout << "resistivity is:" << p << "Ohm.m"<< endl;
+
+
+return 0;
 }
